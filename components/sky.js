1099e7ff95ec1d2acc82d2ce4b563538c459f65a
Jordan Leslie <jordan02leslie@gmail.com>
2024-03-27 12:13:41 -0400

    change background on click

--- components/sky.js
+++ components/sky.js
@@ -12,19 +12,26 @@ AFRAME.registerComponent("background-image", {
     // pick first N places in randomly shuffled array
     const selected_places = shuffled_places.slice(0, NUM_PLACES);
 
+    const images = [];
     // for each place, show all 3 distances in order of decreasing distance
     for (place of selected_places) {
       for (distance of ["long", "med", "close"]) {
         const path = `./images/${place}_${distance}.jpg`;
         // SET SKY BACKGROUND TO IMAGE FROM RANDOMLY SELECTED PATH
-        console.log(path);
-        document.querySelector("a-sky").setAttribute("src", path);
+        images.push(path);
       }
     }
 
     // listen for click on question panel component
+    let index = 0;
+    document.querySelector("a-sky").setAttribute("src", images[0]);
+
+    // change background image on question panel click event
+    // TODO: make this button spam proof
     this.el.sceneEl.addEventListener("answer", () => {
-      console.log("RECEIVED");
+      index++;
+      if (index < images.length)
+        document.querySelector("a-sky").setAttribute("src", images[index]);
     });
   },
 });
